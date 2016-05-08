6808cd869a4468809d62a1811e3b9658d9b6bcd6
Kocha <etskoler42@gmail.com>
2016-05-08 19:40:53 +0200

    hide language link if currect language == language link

--- js/app.js
+++ js/app.js
@@ -3,7 +3,19 @@ $(function() {
     i18n.init(function(err, t) {
         $("#app").i18n();
         var appName = t("app.name");
-
         load:'en'
     });
+});
+
+$(document).ready(function(){
+    var language = i18n.detectLanguage();
+
+    var links = document.getElementsByClassName("lang-link");
+    for(var i=0; i < links.length; i++)
+    {
+        if(links[i].dataset.locale == language)
+        {
+            links[i].style.display = "none";
+        }
+    }
 });
