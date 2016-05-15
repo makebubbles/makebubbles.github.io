719e16c6a69b360f63f35ce4722f8266f81d1b87
Konstantin Srdjanov <srdjanov.konstantin@gmail.com>
2016-05-15 20:46:03 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -6,12 +6,16 @@ $(document).ready(function(){
     i18n.init({
             defaultValue: 'en',
             fallbackLng: 'en',
-            load: 'unspecific'
+            load: 'unspecific',
+            supportedLngs: ['en', 'sr']
         }, function () {
 	       $('#app').i18n(); // Run translation
     });
 
     var language = i18n.detectLanguage();
+    if( language != 'en' && language != 'sr'){
+        language = i18n.setLng('en');
+    }
     $('.language-switcher > span').text(language);
 
     var links = document.getElementsByClassName("lang-link");
